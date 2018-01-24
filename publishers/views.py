7f26ab712d2073199dadb5627880e0ba2ca49a15
NeilsWeb <nrt.turner@gmail.com>
2018-01-24 10:09:50 -0500

    publisher list by alexa rank

--- publishers/views.py
+++ publishers/views.py
@@ -21,8 +21,8 @@ class PublisherListView(ListView):
                 Q(category__icontains=slug)
             )
         else:
-            queryset = Publisher.objects.all().order_by("traffic").order_by('traffic')
-        return queryset
+            queryset = Publisher.objects.all()
+        return queryset.order_by("traffic")
 
 
 class PublisherDetailView(DetailView):
