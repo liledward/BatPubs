bfa53672657b49bec12e46cea88354067ea911ad
NeilsWeb <nrt.turner@gmail.com>
2018-01-24 09:38:52 -0500

    alexa traffic ranks

--- publishers/models.py
+++ publishers/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
+from bs4 import BeautifulSoup
+import requests
+import re
 # Create your models here.
 
 
@@ -20,6 +23,23 @@ class Publisher(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(blank=True)
+    popularity_rank = models.IntegerField(default=None, null=True)
+
+    def save(self, *args, **kwargs):
+        full_url = self.website_address
+        url = re.compile(r"https?://(www\.)?")
+        url = url.sub('', full_url).strip().strip('/')
+        alexia_url = 'http://data.alexa.com/data?cli=10&dat=s&url=' + url
+        html_doc = requests.get(alexia_url)
+        data = html_doc.text
+        soup = BeautifulSoup(data, 'xml')
+        try:
+            add = soup.find("POPULARITY")['TEXT']
+            self.popularity_rank = int(add)
+        except:
+            self.popularity_rank = None
+
+        super(Publisher, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
